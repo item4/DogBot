6a41c921c18f34885526d1a990d640391256b80c
item4 <item4_hun@hotmail.com>
2013-02-05 22:53:06 +0900

    upgrade aws

--- cmd/aws.py
+++ cmd/aws.py
@@ -11,18 +11,30 @@ def cmd_aws(bot,line,args):
 
     data = urllib.urlopen('http://203.247.66.10/cgi-bin/aws/nph-aws_txt_min').read()
     data = data.decode('cp949').replace('\r','').replace('\n','')
+
+    if '>' + args + '<' not in data:
+        data = urllib.urlopen('http://203.247.66.10/cgi-bin/aws/nph-aws_txt_min?&0&MINDB_60M&0&a').read()
+        data = data.decode('cp949').replace('\r','').replace('\n','')
+
+    print args in data
+
     time = re.search(u'\[ 매분관측자료 \] \d+\.\d+\.\d+\.(\d+:\d+)',data)
-    data = re.search(ur'<tr[^>]+><td[^>]+><a[^>]+>\d+</a></td><td[^>]+><a[^>]+>%s</a></td><td[^>]+>\d+m</td><td>(<font color=red>○</font>|-)</td><td>([^<]*)</td><td>[^<]*</td><td>[^<]*</td><td>[^<]*</td><td[^>]*>([^<]*)</td><td>([^<]*)</td><td class=textg>[^<]*</td><td class=textg>([^<]*)</td><td class=textg>([^<]*)</td><td class=textb>[^<]*</td><td class=textb>[^<]*</td><td class=textb>[^<]*</td><td>([^<]*)</td><td>([^<]*)</td><td align=left class=text2 nowrap>([^<]*)</td></tr>' % args,data)
+    data = re.search(ur'<tr[^>]+><td[^>]+><a[^>]+>\d+</a></td><td[^>]+><a[^>]+>%s</a></td><td[^>]+>\d+m</td><td>(<font color=red>○</font>|<font color=blue>●</font>|-)</td><td[^>]*>([^<]*)</td><td[^>]*>[^<]*</td><td[^>]*>[^<]*</td><td[^>]*>[^<]*</td><td[^>]*>([^<]*)</td><td>([^<]*)</td><td class=textg>[^<]*</td><td class=textg>([^<]*)</td><td class=textg>([^<]*)</td><td[^>]*>[^<]*</td><td[^>]*>[^<]*</td><td[^>]*>[^<]*</td><td>([^<]*)</td><td>([^<]*)</td><td align=left class=text2 nowrap>([^<]*)</td></tr>' % args,data)
+    """
+<tr align=center bgcolor=#F2F2F2 class=text><td bgcolor=#BBDDFF><a href='javascript:parent.parent.menu.stn_select(957);'>957</a></td>
+<td bgcolor=#BBDDFF width=50 nowrap><a href='javascript:parent.parent.menu.stn_select(957);'>십이동파</a></td>
+<td align=right width=40>91m</td><td>-</td>
+<td>.</td><td>.</td><td>.</td><td>.</td><td>.</td><td>0.6</td><td class=textg>338.0</td><td class=textg>NNW</td><td class=textg>6.3</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>.</td><td>1016.4</td><td align=left class=text2 nowrap>전라북도 군산시 옥도면 연도리</td></tr>
     """
 
-    """
+
     if data:
         if data.group(1) == '-' or data.group(1) == '.':
             rain = u'모름'
         elif data.group(1) == u'<font color=red>○</font>':
             rain = u'아니오'
         else:
-            rain = u'예(15min:%d/일일:%d)' % (data.group(2),data.group(3))
+            rain = u'예(15min:%s/일일:%s)' % (data.group(2),data.group(3))
 
         temperature = data.group(4)
         wind = u'%s %s㎧' % (data.group(5).replace('N',u'북').\
