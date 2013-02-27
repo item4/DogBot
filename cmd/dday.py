920a2d7db3392f4bfdb66c120f085759988129e7
item4 <item4_hun@hotmail.com>
2013-02-27 23:05:11 +0900

    add dday cmd

---
+++ cmd/dday.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+alias = []
+
+import time
+
+def cmd_dday(bot, line, args):
+    if not args:
+        bot.con.query(
+            'PRIVMSG',
+            line.target,
+            u'해당 날짜까지 며칠 남았는지, 혹은 며칠 지났는지 계산합니다. | usage: ?dday 20131231'
+        )
+        return
+
+    try:
+        dday = time.mktime(time.strptime(args,'%Y%m%d'))
+    except:
+        bot.con.query(
+            'PRIVMSG',
+            line.target,
+            u'멍멍! 날짜가 올바른 형식이 아닙니다.'
+        )
+        return
+
+    today = time.localtime()
+    dday -= time.mktime((today[0],today[1],today[2],0,0,0,today[6],today[7],today[8]))
+
+    if dday > 0:
+        bot.con.query(
+            'PRIVMSG',
+            line.target,
+            u'{0}까지 {1:,}일 남았습니다.'.format(args,int(dday//86400))
+        )
+    else:
+        bot.con.query(
+            'PRIVMSG',
+            line.target,
+            u'{0}로부터 {1:,}일 지났습니다.'.format(args,int(-dday//86400))
+        )
