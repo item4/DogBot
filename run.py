6eaae47c6a51816d14180a25493ae0e0a9dfbc75
item4 <item4_hun@hotmail.com>
2013-06-23 18:15:18 +0900

    add join channel

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from system.bot import *
 def main():
     dog = DogBot()
     dog.load_config()
-    dog.add_connect(u'irc.ozinger.org', 6667, 'utf8', ['#item4','#langdev','#hest','#snoin','#snoin-dev'])
+    dog.add_connect(u'irc.ozinger.org', 6667, 'utf8', ['#item4','#item4-study','#langdev','#hest','#snoin','#snoin-dev','#nagi'])
     dog.start()
 
 if __name__ == '__main__':
