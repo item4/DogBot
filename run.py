bf3b6137a37b162aee63fccc27c720cd7021daf0
item4 <item4_hun@hotmail.com>
2013-06-29 21:00:20 +0900

    no change. just test

--- run.py
+++ run.py
@@ -3,10 +3,10 @@
 from system.bot import *
 
 def main():
-    dog = DogBot()
-    dog.load_config()
-    dog.add_connect(u'irc.ozinger.org', 6667, 'utf8', ['#item4','#item4-study','#langdev','#hest','#snoin','#snoin-dev','#nagi'])
-    dog.start()
+        dog = DogBot()
+        dog.load_config()
+        dog.add_connect(u'irc.ozinger.org', 6667, 'utf8', ['#item4','#item4-study','#langdev','#hest','#snoin','#snoin-dev','#nagi'])
+        dog.start()
 
 if __name__ == '__main__':
     main()
