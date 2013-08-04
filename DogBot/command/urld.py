c4b45942ff09a82568261ae15b313f6d8f7fe8c2
item4 <item4_hun@hotmail.com>
2013-08-04 12:01:31 +0900

    fix url decode command

--- DogBot/command/urld.py
+++ DogBot/command/urld.py
@@ -5,6 +5,7 @@ handler = []
 
 import urllib
 
+
 def cmd_urld(bot, line, args):
     if not args:
         bot.con.query(
@@ -31,15 +32,13 @@ def cmd_urld(bot, line, args):
                 line.target,
                 u'멍멍! 인코딩 파악에 실패했습니다.'
             )
-
-    #print repr(data)
+            return
 
     bot.con.query(
         'PRIVMSG',
         line.target,
         u'[%s] %s: %s' % (encoding, args, data)
     )
-
 
 
 def main():
