9d4b5068b2e7af81a8ee9188a507e581d25b4cc4
item4 <item4_hun@hotmail.com>
2013-08-04 12:12:34 +0900

    upgrade sub search command

--- DogBot/command/sub.py
+++ DogBot/command/sub.py
@@ -6,13 +6,15 @@ handler = []
 import urllib
 import re
 
+wday = [u'일', u'월', u'화', u'수', u'목', u'금', u'토']
+
 
 def cmd_sub(bot, line, args):
     if not args:
         bot.con.query(
             'PRIVMSG',
             line.target,
-            u'애니 자막 현황을 출력합니다. | usage: ?anitime 이나즈마 일레븐'
+            u'애니 자막 현황을 출력합니다. | usage: ?sub 이나즈마 일레븐'
         )
         return
 
@@ -45,8 +47,14 @@ def cmd_sub(bot, line, args):
             u'멍멍! 그런 애니를 찾을 수 없어요!'
         )
     else:
-        data = data[6:-3].split('|')[1:]
-        for l in data:
+        data = data[6:-3].split('|')
+        bot.con.query(
+            'PRIVMSG',
+            line.target,
+            u'[%s] %s - http://%s' %
+            (wday[i], args, data[0])
+        )
+        for l in data[1:]:
             temp, nick, url = l.split(',')
             ep = temp[:3]
             month = temp[3:5]
