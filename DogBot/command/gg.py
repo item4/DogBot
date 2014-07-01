4c53442ef6216f899567c147dbbf0f8b7ed2ae1d
item4 <item4_hun@hotmail.com>
2014-07-01 12:28:48 +0900

    fix bug - can't parse first element

--- DogBot/command/gg.py
+++ DogBot/command/gg.py
@@ -9,7 +9,7 @@ import re
 import HTMLParser
 
 
-base_pattern = re.compile(r'<li class="g"[^>]*><!--m-->(.+?)<!--n--></li>')
+base_pattern = re.compile('<li class="g"[^>]*>(?P<div><div>)?<!--m-->(.+?)(?(div)</li><!--n-->|<!--n--></li>)')
 temp_pattern = re.compile('<h3 class="r"><a href="([^"]+)"[^>]+>(.+?)</a></h3>')
 desciption_pattern = re.compile('<span class="st">(.+?)</span>')
 
@@ -46,6 +46,7 @@ def cmd_gg(bot, line, args):
         )
     else:
         result = base_pattern.findall(data[f:])[:3]
+        print result
 
         if not result:
             temp = data.find('<li class="g">') == -1 or data.find('<li class="g" ') == -1
@@ -63,7 +64,7 @@ def cmd_gg(bot, line, args):
                 )
             return
 
-        for x in result:
+        for _, x in result:
             temp = temp_pattern.search(x)
             url, title = temp.group(1), temp.group(2)
             try:
